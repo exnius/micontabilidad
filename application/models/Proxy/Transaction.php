2eb4f35370422fe0c454213b6dbdb26532ccf719
pamito <pablomayorgqa@gmail.com>
2012-12-11 12:56:04 -0500

    proxy and vo

---
+++ application/models/Proxy/Transaction.php
@@ -0,0 +1,17 @@
+<<?php
+class Proxy_Transaction extends Contabilidad_Proxy
+{
+    
+    protected static $_instance = null;
+
+    /**
+     * @return Public Proxy_Primitives
+     */
+    public static function getInstance ()
+    {
+        if (null === self::$_instance) {
+            self::$_instance = new self('transaccion', 'VO_User');
+        }
+        return (self::$_instance);
+    }
+}
---
+++ application/models/VO/ViewType.php
@@ -0,0 +1,4 @@
+<?php
+class VO_ViewType extends Zend_Db_Table_Row {
+    
+}
