b8f8d46e61b3726b222f1f164c64865d994708fe
Andres <ayn_nian@hotmail.com>
2012-12-12 10:06:30 -0500

    some views and session system

--- application/modules/public/php/controllers/IndexController.php
+++ application/modules/public/php/controllers/IndexController.php
@@ -7,11 +7,15 @@ class IndexController extends Zend_Controller_Action
     {
         /* Initialize action controller here */
     }
+    
+    public function indexAction()
+    {
+        $this->_forward("home");
+    }
 
     public function homeAction()
     {
-//        var_dump("heheheh");
-        // action body
+        
     }
     
     public function termsAction()  
