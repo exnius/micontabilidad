8e5795b6589da4607a98a0ff1eaa9284f7bc8519
HellRod <krakend2@gmail.com>
2012-12-13 13:02:08 -0500

    createNickname

--- application/models/Proxy/User.php
+++ application/models/Proxy/User.php
@@ -18,10 +18,36 @@ class Proxy_User extends Contabilidad_Proxy
         $row->password = $params['password'];
         $row->email = $params['email'];
         $row->id_currency = 1;
+        $row->nickname = $this->createNickname($row->full_name);
         $row->save();
     }
     
     public function findByEmail($email){
         return $this->getTable()->fetchRow("email = '$email'");
     }
+    
+    public function createNickname ($nickname){
+        
+        $nickname = trim ($nickname);
+        $nickname = preg_replace('/(À|Á|Â|Ã|Ä|Å|à|á|â|ã|ä|å|@)/','a',$nickname);
+        $nickname = preg_replace('/(È|É|Ê|Ë|è|é|ê|ë)/','e',$nickname);
+        $nickname = preg_replace('/(Ì|Í|Î|Ï|ì|í|î|ï)/','i',$nickname);
+        $nickname = preg_replace('/(Ò|Ó|Ô|Õ|Ö|Ø|ò|ó|ô|õ|ö|ø)/','o',$nickname);
+        $nickname = preg_replace('/(Ù|Ú|Û|Ü|ù|ú|û|ü)/','u',$nickname);
+        $nickname = preg_replace('/(Ç|ç)/','c',$nickname);
+        $nickname = preg_replace('/(Ñ|ñ)/','n',$nickname);
+        $nickname = preg_replace('/(ÿ|Ý)/','y',$nickname);
+        $nickname = preg_replace('/(\^)/',' ',$nickname);
+        $nickname = strtolower ($nickname);
+        $nickname = preg_replace('/\s+/',' ', $nickname);
+        $nickname = preg_replace("[ ]",".",$nickname);
+        
+	    return $nickname;
+	}
+    
+    private function findByNickname ($nickname){
+        
+    }
+
+    
 }
