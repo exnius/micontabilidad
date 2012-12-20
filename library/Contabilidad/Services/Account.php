b1bf2959e916733f8259eb7e7da957687bde26ea
HellRod <krakend2@gmail.com>
2012-12-20 12:21:58 -0500

    Service deleteAccount

--- library/Contabilidad/Services/Account.php
+++ library/Contabilidad/Services/Account.php
@@ -2,6 +2,7 @@
 class Contabilidad_Services_Account extends Contabilidad_Services_Abstract {
     const NOT_ALL_PARAMS = "not all params";
     const NOT_AUTHENTICATED = "not authenticated";
+    const UNSELECTED_ACCOUNT = "unselected account";
 
     public function createAccount($params){
         $resp = array("result" => "failure", "reason" => self::NOT_ALL_PARAMS);
@@ -17,6 +18,22 @@ class Contabilidad_Services_Account extends Contabilidad_Services_Abstract {
            }  else {
                $resp["reason"] = self::NOT_AUTHENTICATED;
            }
+        }
+        return $resp;
+    }
+    
+    public function deleteAccount ($id){
+        
+        $resp = array("result" => "failure", "reason" => self::UNSELECTED_ACCOUNT);
+        $account = Proxy_Account::getInstance()->findById($id);
+        if ($id){
+            if ($account->id_user == Contabilidad_Auth::getInstance()->getUser()->id){
+                $account->delete();
+                $resp["result"] = "success";
+                $resp["reason"] = "OK";
+            } else {
+                $resp["reason"] = "not autthorized usser";
+            }
         }
         return $resp;
     }
