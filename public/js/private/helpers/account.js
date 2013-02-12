67912b57ff391b05e455600aba3216e0752ad6c9
Andres <ayn_nian@hotmail.com>
2013-02-12 08:09:13 -0500

    minor

--- public/js/private/helpers/account.js
+++ public/js/private/helpers/account.js
@@ -48,7 +48,7 @@ QHelpers.account.showBalancePopup = function ($el, account){
         $div.find(".title").html(Contabilidad.tr("Crear balance"));
         $div.find("input[type='submit']").val(Contabilidad.tr("Crear"));
         //image
-        $div.find("#account-picture").attr("src", "http://img.uefa.com/imgml/TP/players/14/2013/324x324/250011928.jpg");
+        $div.find("#account-picture").attr("src", LINKS_URL + "/quantups_pictures/budget.png");
         //date ini
         $div.find("input[name='date_ini']")
         .val(Contabilidad.toDate(dateIni))
